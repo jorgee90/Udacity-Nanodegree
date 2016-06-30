2c88b62081128edec7a81ca583355eb5e0803bb7
Samsruti Dash <sam.sipun@gmail.com>
2016-06-30 18:28:30 +0530

    Book App

--- BookApp/app/src/main/java/com/samsrutidash/bookapp/MainActivity.java
+++ BookApp/app/src/main/java/com/samsrutidash/bookapp/MainActivity.java
@@ -2,13 +2,16 @@ package com.samsrutidash.bookapp;
 
 import org.json.JSONException;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.AsyncTask;
 
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListAdapter;
@@ -24,8 +27,9 @@ import org.json.JSONException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     ListView list;
+    public TextView emptyView;
     private static String urlString;
     //JSON Node Names
     private static final String TAG_TITLE = "title";
@@ -40,12 +44,14 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         Button btn = (Button) findViewById(R.id.btn);
+        assert btn != null;
+        emptyView = (TextView) findViewById(R.id.empty);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText input = (EditText) findViewById(R.id.input);
                 String inputQuery = input.getText().toString();
-
+                inputQuery = inputQuery.replace(" ","+");
                 String APIKEY = "&key=AIzaSyCvvuHSz0nYnU5awMlCij1IhN-sHX1Q1O4";
                 urlString = "https://www.googleapis.com/books/v1/volumes?q="+inputQuery+"&orderBy=newest";
                 Log.v("URL:",urlString);
@@ -54,7 +60,14 @@ public class MainActivity extends Activity {
         });
     }
 
+
     private class ProcessJSON extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this, "Please Wait.. Loading...", Toast.LENGTH_SHORT).show();
+        }
+
         protected String doInBackground(String... strings) {
             String stream = null;
             String urlString = strings[0];
@@ -67,77 +80,73 @@ public class MainActivity extends Activity {
         }
 
         protected void onPostExecute(String stream) {
-//            Log.v("URL: ", stream);
-
-            //tv.setText(stream);
-
-            /*
-                Important in JSON DATA
-                -------------------------
-                * Square bracket ([) represents a JSON array
-                * Curly bracket ({) represents a JSON object
-                * JSON object contains key/value pairs
-                * Each key is a String and value may be different data types
-             */
-
-            //..........Process JSON DATA................
+            list = (ListView) findViewById(R.id.list);
             if (stream != null) {
                 try {
                     // Get the full HTTP Data as JSONObject
                     JSONObject reader = new JSONObject(stream);
+                    int totalItems = reader.getInt("totalItems");
+                    if (totalItems==0){
+                        list.setVisibility(View.INVISIBLE);
 
+                        Toast.makeText(MainActivity.this, "No Results Found", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        emptyView.setText("");
+                        // Get the JSONArray weather
+                        JSONArray bookArray = reader.getJSONArray("items");
+                        // Get the weather array first JSONObject
+                        for (int i = 0; i < bookArray.length(); i++) {
 
-                    // Get the JSONArray weather
-                    JSONArray weatherArray = reader.getJSONArray("items");
-                    // Get the weather array first JSONObject
-                    for (int i = 0; i < weatherArray.length(); i++) {
-
-                        JSONObject weather_object_0 = weatherArray.getJSONObject(i);
-                        String weather_0_id = weather_object_0.getString("id");
-
-                        JSONObject BookDetails= weather_object_0.getJSONObject("volumeInfo");
-                        String title = BookDetails.getString("title");
-                        String description = BookDetails.getString("description");
-                        String authors = BookDetails.getString("authors");
-                        authors = authors.replace("[", "");
-                        authors = authors.replace("]", "");
-                        Log.v(TAG_TITLE,title);
-
-                        Log.v(TAG_AUTHORS,authors);
-
-                        // Adding value HashMap key => value
-
-                        HashMap<String, String> map = new HashMap<String, String>();
-                        map.put(TAG_TITLE, title);
-                        map.put(TAG_AUTHORS, authors);
-                        map.put(TAG_DESCRIPTION,description);
-
-                        booklist.add(map);
-                        list = (ListView) findViewById(R.id.list);
-
-                        ListAdapter adapter = new SimpleAdapter(MainActivity.this,
-                                booklist,
-                                R.layout.listview_layout,
-                                new String[]{TAG_TITLE, TAG_AUTHORS,TAG_DESCRIPTION},
-                                new int[]{ R.id.bookTitle, R.id.bookAuthors,}
-                        );
-
-                        list.setAdapter(adapter);
-                        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(AdapterView<?> parent, View view,
-                                                    int position, long id) {
-                                Toast.makeText(MainActivity.this, "Description:\n  " + booklist.get(+position).get("description"), Toast.LENGTH_LONG).show();
+                            JSONObject BookObject = bookArray.getJSONObject(i);
+                            String title, authors;
+                            JSONObject BookDetails= BookObject.getJSONObject("volumeInfo");
+                            if (BookDetails.has("authors")){
+                                authors = (BookDetails.getString("authors"));
+                                authors = authors.replace("[", "");
+                                authors = authors.replace("]", "");
                             }
-                        });
+                            else{
+                                authors = "";
+
+                            }
+                            title = BookDetails.getString("title");
+
+                            Log.v(TAG_TITLE,title);
+                            Log.v(TAG_AUTHORS,authors);
+
+
+                            // Adding value HashMap key => value
+
+                            HashMap<String, String> map = new HashMap<String, String>();
+                            map.put(TAG_TITLE, title);
+                            map.put(TAG_AUTHORS, authors);
+
+                            booklist.add(map);
+
+                            ListAdapter adapter = new SimpleAdapter(MainActivity.this,
+                                    booklist,
+                                    R.layout.listview_layout,
+                                    new String[]{TAG_TITLE, TAG_AUTHORS},
+                                    new int[]{ R.id.bookTitle, R.id.bookAuthors}
+                            );
+
+                            list.setAdapter(adapter);
+                            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view,
+                                                        int position, long id) {
+                                    Toast.makeText(MainActivity.this, "Description:\n  " + booklist.get(+position), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        }
 
                     }
-
-
                     // process other data as this way..............
-
-                } catch (JSONException e) {
+                }catch (JSONException e) {
                     e.printStackTrace();
+
                 }
 
             } // if statement end
